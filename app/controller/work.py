669a3ed468d01dec8bf6e8cc63960d6f24df91d1
Sstark97 <aitorscinfo@gmail.com>
2022-07-02 12:41:16 +0100

    Add cv check with db

--- app/controller/work.py
+++ app/controller/work.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, logout_user, current_user
 from app.forms.forms import AccountForm, DeleteAccountForm
 from app.model.db_config import db_session
-from app.model.models import Work
+from app.model.models import Work, Cv
 
 work = Blueprint('work', __name__)
 
@@ -12,12 +12,13 @@ work = Blueprint('work', __name__)
 @login_required
 def work_index():
     """ Página de Acerca de """
+    curriculum = db_session.query(Cv).filter(Cv.user_email == current_user.email).first()
 
     context = {
         'title': 'Experiencia de Trabajo',
         'action': url_for('work.work_index'),
         'type': 'Experiencia de Trabajo',
-        'cv': True,
+        'cv': curriculum,
         'form': ""
     }
 
