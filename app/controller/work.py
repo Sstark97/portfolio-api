4b00d5dd7f2503ba567171b901c7dea493596f26
Sstark97 <aitorscinfo@gmail.com>
2022-07-06 15:03:14 +0100

    Finish work edit

--- app/controller/work.py
+++ app/controller/work.py
@@ -46,15 +46,69 @@ def work_new():
     if work_form.validate_on_submit():
         start_date = date(work_form.start_date.data.year,
                           work_form.start_date.data.month, work_form.start_date.data.day)
-        final_date = date(work_form.final_date.data.year,
-                          work_form.final_date.data.month, work_form.final_date.data.day)
+        final_date = None
+        current = False
+
+        if work_form.final_date.data:
+            final_date = date(work_form.final_date.data.year,
+                              work_form.final_date.data.month, work_form.final_date.data.day)
+            current = True
 
         new_work = Work(position=work_form.position.data, company=work_form.company.data, description=work_form.description.data,
-                    start_date=start_date, final_date=final_date, current=work_form.current.data, user_email=current_user.email)
+                    start_date=start_date, final_date=final_date, current=current, user_email=current_user.email)
         
         db_session.add(new_work)
         db_session.commit()
 
         return redirect(url_for('work.work_index'))
     
+    return render_template('forms.html', **context)
+
+@work.route('/work/edit/<int:work_id>', methods=['GET', 'POST'])
+@login_required
+def work_edit(work_id):
+    """ Página de edición de experiencia de trabajo """
+
+    work_to_edit = db_session.query(Work).filter_by(id=work_id).first()
+
+    if request.method == 'POST':
+        work_form = WorkForm(request.form)
+    else:
+        work_form = WorkForm()
+        work_form.position.data = work_to_edit.position
+        work_form.company.data = work_to_edit.company
+        work_form.description.data = work_to_edit.description
+        work_form.start_date.data = work_to_edit.start_date
+        work_form.final_date.data = work_to_edit.final_date
+
+    context = {
+        'title': 'Editar Experiencia de Trabajo',
+        'form': work_form,
+        'action': url_for('work.work_edit', work_id=work_id),
+        'data': work_to_edit,
+    }
+
+    if work_form.validate_on_submit():
+        start_date = date(work_form.start_date.data.year,
+                          work_form.start_date.data.month, work_form.start_date.data.day)
+        final_date = None
+        current = False
+
+        if work_form.final_date.data:
+            final_date = date(work_form.final_date.data.year,
+                              work_form.final_date.data.month, work_form.final_date.data.day)
+            current = True
+            
+        db_session.query(Work).filter_by(id=work_id).update({
+            'position': work_form.position.data,
+            'company': work_form.company.data,
+            'description': work_form.description.data,
+            'start_date': start_date,
+            'final_date': final_date,
+            'current': current,
+        })
+        db_session.commit()
+
+        return redirect(url_for('work.work_index'))
+
     return render_template('forms.html', **context)
--- app/forms/work_form.py
+++ app/forms/work_form.py
@@ -1,7 +1,8 @@
 """ Formulario para añadir experiencia laboral """
+from datetime import date
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms import StringField, SubmitField, DateField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, Optional, ValidationError
 
 class WorkForm(FlaskForm):
 
@@ -15,13 +16,11 @@ class WorkForm(FlaskForm):
 
     start_date = DateField('Fecha de Inicio', validators=[DataRequired(message="La fecha de inicio es requerida")])
 
-    final_date = DateField('Fecha de Finalización')
+    final_date = DateField('Fecha de Finalización', validators=[Optional()])
 
     def validate_final_date(self,final_date):
         """ Función que valida que la fecha de finalización sea mayor que la de inicio """
-        if final_date.data and self.start_date.data > final_date.data:
+        if isinstance(final_date.data,date) and self.start_date.data > final_date.data:
             raise ValidationError("La fecha de finalización debe ser mayor que la de inicio")
-
-    current = BooleanField('Actual')
 
     submit = SubmitField('Añadir')
