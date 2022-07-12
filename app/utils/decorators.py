bb5fe231afc596c022015dc8ca170634b1959b34
Sstark97 <aitorscinfo@gmail.com>
2022-07-12 11:44:21 +0100

    Add login_redirect decorator

---
+++ app/utils/decorators.py
@@ -0,0 +1,14 @@
+""" Archivo con decoradores custom """
+from flask import redirect, url_for
+from flask_login import current_user
+
+def login_redirect(func):
+    """ Redirecciona a la página de inicio si no está logueado """
+    def wrapper(*args, **kwargs):
+    
+        if current_user.is_authenticated:
+            return redirect(url_for('home'))
+        return func(*args, **kwargs)
+        
+    wrapper.__name__ = func.__name__  
+    return wrapper
