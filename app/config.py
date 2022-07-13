dd9dbdf870979d362d2e0e4bca53028450ecf654
Sstark97 <aitorscinfo@gmail.com>
2022-07-13 13:00:20 +0100

    Change config.py

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ class Config:
     load_dotenv()
 
     SECRET_KEY = getenv('SECRET_KEY')
+    DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///portfolio.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static/img'
--- index.py
+++ index.py
@@ -25,4 +25,4 @@ if __name__ == '__main__':
     if environ.get('APP_LOCATION') == 'heroku':
         app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
     else:
-        app.run(host='localhost', port=8080, debug=True)
+        app.run(host='localhost', port=8080)
