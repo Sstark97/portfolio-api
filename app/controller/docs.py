daf168877478d1cb034336049f372651b4ce6ec4
Sstark97 <aitorscinfo@gmail.com>
2022-07-20 11:49:27 +0100

    Finish docs

--- app/controller/docs.py
+++ app/controller/docs.py
@@ -8,8 +8,74 @@ docs = Blueprint('docs', __name__)
 def docs_index():
     """ Documentación de la API """
 
-    data = [{'title': 'Cuenta', 'routes': [{'method': 'GET', 'color': 'bg-primary',
-                                            'endpoint': '/api/v1/user', 'description': 'Prueba'}, ]}]
+    data = [
+        {
+            'routes': [
+                {
+                    'method': 'GET', 
+                    'color': 'bg-primary',
+                    'endpoint': '/api/v1/user', 
+                    'description': 'Obtiene los datos de la Cuenta'
+                },
+                {
+                    'method': 'GET', 
+                    'color': 'bg-primary',
+                    'endpoint': '/api/v1/user/all', 
+                    'description': 'Obtiene todos los datos de la cuenta, todo el PortFolio'  
+                }
+            ],
+        },
+        {
+            'routes': [
+                {
+                    'method': 'GET', 
+                    'color': 'bg-primary',
+                    'endpoint': '/api/v1/projects', 
+                    'description': 'Obtiene todos los proyectos de una Cuenta'
+                }
+            ]
+        },
+        {
+            'routes': [
+                {
+                    'method': 'GET', 
+                    'color': 'bg-primary',
+                    'endpoint': '/api/v1/education', 
+                    'description': 'Obtiene todos los Datos Académicos de una Cuenta'
+                }
+            ]
+        },
+        {
+            'routes': [
+                {
+                    'method': 'GET', 
+                    'color': 'bg-primary',
+                    'endpoint': '/api/v1/work', 
+                    'description': 'Obtiene todos la Experiencia Laboral de una Cuenta'
+                }
+            ]
+        },
+        { 
+            'routes': [
+                {
+                    'method': 'GET', 
+                    'color': 'bg-primary',
+                    'endpoint': '/api/v1/skills', 
+                    'description': 'Obtiene todos las Habilidades de una Cuenta'
+                }
+            ]
+        },
+        {
+            'routes': [
+                {
+                    'method': 'GET', 
+                    'color': 'bg-primary',
+                    'endpoint': '/api/v1/hobbies', 
+                    'description': 'Obtiene todos los Hobbies de una Cuenta'
+                }
+            ]
+        },
+    ]
 
     context = {
         'title': 'Documentación',
