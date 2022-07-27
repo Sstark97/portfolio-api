f995d31220553f3ff3f4d4eaab2b1dba585e220d
Sstark97 <aitorscinfo@gmail.com>
2022-07-27 09:20:02 +0100

    Migrate to PostGreSQL

--- app/model/db_config.py
+++ app/model/db_config.py
@@ -1,9 +1,13 @@
 """ Archivo de configuración de la Base de Datos"""
+from os import getenv
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///portfolio.db?check_same_thread=False')
+load_dotenv()
+DATA_BASE_URI = getenv('DATABASE_URL')
+engine = create_engine(f'{DATA_BASE_URI}/portfolio.db?check_same_thread=False')
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
