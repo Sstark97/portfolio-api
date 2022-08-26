ceaf793973a2a0c39b0a34b8b6664a1eaa10f08c
Sstark97 <aitorscinfo@gmail.com>
2022-08-26 22:26:05 +0100

    Add teardown_request

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import unittest
 from flask import render_template
 from app import create_app
 from app.config import Config
+from app.model.db_config import db_session
 
 app = create_app()
 app.config.from_object(Config())
@@ -19,6 +20,14 @@ def index():
     """Página de Inicio"""
 
     return render_template('home.html', title='Bienvenido')
+
+@app.teardown_request
+def session_clear(exception=None):
+    """Cierra la sesión de la Base de Datos"""
+    db_session.remove()
+
+    if exception and db_session.is_active:
+        db_session.rollback()
 
 if __name__ == '__main__':
     app.run()
